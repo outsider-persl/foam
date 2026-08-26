89c27c887b42dcd184a2cd1b9798c7a45ec6f924
outsider <outsider.persl@gmail.com>
2026-08-26 16:36:45 +0800

    first commit

---
+++ src/main/java/com/outsider/foam/FoamApplication.java
@@ -0,0 +1,13 @@
+package com.outsider.foam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/outsider/foam/FoamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outsider.foam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
